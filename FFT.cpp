a7fcd1f817d77b72ce95a5a04e9107b8c5042efa
Rafy Bhuiyan <rafybhuiyan55@gmail.com>
2025-04-13 01:35:53 +0600

    Create FFT.cpp

---
+++ FFT.cpp
@@ -0,0 +1,69 @@
+typedef double ld;
+typedef complex <ld> cplx;
+
+const ld PI = acosl(-1);
+const int MOD = 1e9 + 7;
+const int N = (1 << 20) + 69;
+
+int rev[N];
+cplx w[N];
+
+void prepare (int n)
+{
+    int sz = __builtin_ctz(n);
+    for (int i = 1; i < n; ++i) rev[i] = (rev[i >> 1] >> 1) | ((i & 1) << (sz - 1));
+    w[0] = 0, w[1] = 1, sz = 1;
+    while (1 << sz < n)
+    {
+        ld ang = 2 * PI / (1 << (sz + 1));
+        cplx w_n = cplx(cosl(ang), sinl(ang));
+        for (int i = 1 << (sz - 1); i < (1 << sz); ++i)
+        {
+            w[i << 1] = w[i], w[i << 1 | 1] = w[i] * w_n;
+        }
+        ++sz;
+    }
+}
+
+void fft (cplx *a, int n)
+{
+    for (int i = 1; i < n - 1; ++i)
+    {
+        if (i < rev[i]) swap(a[i], a[rev[i]]);
+    }
+    for (int h = 1; h < n; h <<= 1)
+    {
+        for (int s = 0; s < n; s += h << 1)
+        {
+            for (int i = 0; i < h; ++i)
+            {
+                cplx &u = a[s + i], &v = a[s + i + h], t = v * w[h + i];
+                v = u - t, u = u + t;
+            }
+        }
+    }
+}
+
+static cplx f[N];
+
+vector <int> multiply (vector <int> a, vector <int> b)
+{
+    int n = a.size(), m = b.size();
+    int sz = n + m - 1;
+    while (sz & (sz - 1)) sz = (sz | (sz - 1)) + 1;
+    prepare(sz);
+    for (int i = 0; i < sz; ++i) f[i] = cplx(i < n ? a[i] : 0, i < m ? b[i] : 0);
+    fft(f, sz);
+    for (int i = 0; i <= (sz >> 1); ++i)
+    {
+        int j = (sz - i) & (sz - 1);
+        cplx x = (f[i] * f[i] - conj(f[j] * f[j])) * cplx(0, -0.25);
+        f[j] = x, f[i] = conj(x);
+    }
+    fft(f, sz);
+    vector <int> c(n + m - 1);
+    for(int i = 0; i < n + m - 1; ++i) c[i] = (f[i].real() / sz + 0.5);
+    for(int i = 0; i < n + m - 1; ++i) c[i] = (c[i] > 0);
+    return c;
+}
+
