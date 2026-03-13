f6dbf5782d9abdc4b2756940d782da29cba039d2
Rafy Bhuiyan <rafybhuiyan23@gmail.com>
2026-03-14 00:31:19 +0600

    Refactor Combinatorics class for new MOD and MX values

--- Combinatorics.cpp
+++ Combinatorics.cpp
@@ -1,34 +1,41 @@
-const ll MOD = 1e9+7;
-const ll MX = 1e6;
+const int MOD = 998244353;
+const int MX = 5000001;
 struct Combinatorics
 {
-    ll n,mod;
-    vector<ll> _fac, _inv, _ifac;
-    Combinatorics(ll n, ll mod) : n(n), mod(mod),_fac(n+1), _inv(n+1), _ifac(n+1)
+    int n, mod;
+    vector<int> _fac, _inv, _ifac;
+    Combinatorics(int n, int mod) : n(n), mod(mod), _fac(n + 1),_inv(n + 1), _ifac(n + 1)
     {
-        for (ll i = 0; i <= 1; ++i) _fac[i] = _inv[i] = _ifac[i] = 1;
-        for (ll i = 2; i <= n; ++i) _fac[i] = (_fac[i-1] * i) % mod;
-        for (ll i = 2; i <= n; ++i) _inv[i] = mod - ((mod/i * _inv[mod % i]) % mod);
-        for (ll i = 2; i <= n; ++i) _ifac[i] = (_ifac[i-1] * _inv[i]) % mod;
+        for (int i = 0; i <= 1; ++i) _fac[i] = _inv[i] = _ifac[i] = 1;
+        for (int i = 2; i <= n; ++i) _fac[i] = (_fac[i - 1] * i)% mod;
+        for (int i = 2; i <= n; ++i) _inv[i] = mod - ((mod / i *_inv[mod % i]) % mod);
+        for (int i = 2; i <= n; ++i) _ifac[i] = (_ifac[i - 1] * _inv[i]) % mod;
     }
-    ll fac(ll n)
+    int fac(int n)
     {
         return _fac[n];
     }
-    ll ifac(ll n)
+    int ifac(int n)
     {
         return _ifac[n];
     }
-    ll inv(ll n)
+    int inv(int n)
     {
         return _inv[n];
     }
-    ll ncr(ll n, ll r)
+    int ncr(int n, int r)
     {
         if (r > n) return 0;
-        ll ans = fac(n);
+        int ans = fac(n);
         ans = (ans * ifac(r)) % mod;
-        ans = (ans * ifac(n-r)) % mod;
+        ans = (ans * ifac(n - r)) % mod;
+        return ans;
+    }
+    int npr(int n, int r)
+    {
+        if (r > n) return 0;
+        int ans = fac(n);
+        ans = (ans * ifac(n - r)) % mod;
         return ans;
     }
 } comb(MX, MOD);
